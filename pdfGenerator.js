855210fbfb48dd1a99bba1b56779d1f34ef6babe
arwom1 <arwom.me@gmail.com>
2026-04-21 11:44:24 +0530

    Update pdfGenerator.js

--- pdfGenerator.js
+++ pdfGenerator.js
@@ -1,15 +1,19 @@
 const { execSync } = require('child_process');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
 async function generateNotesPDF({ subject, time, date, notesMarkdown, pdfPath }) {
   const tmpMd = pdfPath.replace('.pdf', '_content.txt');
   fs.writeFileSync(tmpMd, notesMarkdown, 'utf8');
+  
   const scriptPath = path.join(__dirname, 'make_pdf.py');
+  
+  // Use 'python3' specifically
   execSync(`python3 "${scriptPath}" "${subject}" "${time}" "${date}" "${tmpMd}" "${pdfPath}"`, {
     timeout: 30000,
   });
-  fs.unlinkSync(tmpMd);
+
+  if (fs.existsSync(tmpMd)) fs.unlinkSync(tmpMd);
 }
 
 module.exports = { generateNotesPDF };
